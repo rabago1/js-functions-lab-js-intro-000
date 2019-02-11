954b859923fcae566bcbfe51488f33af3b389a6d
Pat Rabago <patriciorabago@gmail.com>
2019-02-11 01:21:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ function happyHolidaysTo(name) {
 function happyHolidaysTo(holiday,name) {
   return `Happy ${holiday}, ${name}!`;
 }
+function holidayCountdown(holiday,days) {
+  return `It's ${days} days until ${holiday}!`
+}
